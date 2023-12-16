53163119359fce3054335209535617db838b3a11
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-12-16 12:51:51 +0100

    Bubble Sort Algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * swap - Swap the values of two integers.
+ * @x_val: Pointer to the first integer.
+ * @y_val: Pointer to the second integer.
+ */
+void swap(int *x_val, int *y_val)
+{
+	int buffer;
+
+	buffer = *x_val;
+	*x_val = *y_val;
+	*y_val = buffer;
+}
+
+/**
+ * bubble_sort - Sort an array of integers using the Bubble Sort algorithm.
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	bool swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				swapped = true;
+			}
+			if (swapped == true)
+				print_array(array, size);
+		}
+
+		if (swapped == false)
+			break;
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+0x1B. C - Sorting algorithms & Big O is a team project by Saheed Kehinde Yusuf and Barakat Mohammed
