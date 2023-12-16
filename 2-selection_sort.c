6b1ce3ba8b084bf5caa5c23a3cc76ee7e65a8ce0
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-12-16 20:41:34 +0100

    Selection Sort Algorithm++

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,15 @@
 #include "sort.h"
 
+/**
+ * swap - Swaps the values of two integers.
+ * @x_val: Pointer to the first integer.
+ * @y_val: Pointer to the second integer.
+ *
+ * Description: This function swaps the values of two integers.
+ */
+
+
+
 void swap(int *x_val, int *y_val)
 {
 	int buffer;
@@ -8,6 +18,17 @@ void swap(int *x_val, int *y_val)
 	*x_val = *y_val;
 	*y_val = buffer;
 }
+
+/**
+ * selection_sort - Sorts an array of integers using Selection Sort algorithm.
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ *
+ * Description: Function sorts an array of integers in ascending order using
+ * the Selection Sort algorithm. It iteratively finds the minimum element in
+ * the unsorted part of the array and swaps it with the first unsorted element.
+ * The process is repeated until the entire array is sorted.
+ */
 
 void selection_sort(int *array, size_t size)
 {
